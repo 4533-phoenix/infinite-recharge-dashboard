edaa52b1d0dd880064b9fa92610d39b9935e30e6
Adam Brightwell <adam.brightwell@gmail.com>
2020-02-14 10:11:09 -0500

    add comments to frame.py to describe how the frame is built

--- frame.py
+++ frame.py
@@ -27,6 +27,23 @@ class MainFrame(wx.Frame):
         self.Center()
 
     def build(self):
+
+        # The main panel of the frame is laid out and sized by a Gridbag Sizer.
+        # Below is the mapping of each component within that sizer.
+        #
+        # |   | 0 | 1 | 2 |
+        # |---|---|---|---|
+        # | 0 | T | T | T |
+        # | 1 | A | D | I |
+        # | 2 | A | D | I |
+        # | 3 | A | C | I |
+        # |---|---|---|---|
+        #
+        # T = Game Timer
+        # A = Auto Chooser
+        # D = Driver Assist
+        # C = Driver Assist Camera Buttons
+        # I = Intake Status
         gbs = wx.GridBagSizer(5, 4)
 
         mainPanel = wx.Panel(self)
@@ -35,8 +52,10 @@ class MainFrame(wx.Frame):
         self.intakeStatus = IntakeStatusPanel(mainPanel)
         self.intakeStatus.update([True, True, False, False, True])
 
+        # Get the configuration from the global 'Application'.
         config = DashboardApp.Get().config
 
+        # Build and add the Game Timer Panel.
         gbs.Add(
             GameTimerPanel(mainPanel),
             wx.GBPosition(0,0),
@@ -44,6 +63,7 @@ class MainFrame(wx.Frame):
             flag=wx.EXPAND
         )
 
+        # Build and add the Auto Chooser Panel.
         gbs.Add(
             AutoChooserPanel(mainPanel, config),
             wx.GBPosition(1,0),
@@ -51,6 +71,7 @@ class MainFrame(wx.Frame):
             flag=wx.EXPAND
         )
 
+        # Build and add the Driver Assist Panel.
         gbs.Add(
             DriverAssistPanel(mainPanel),
             wx.GBPosition(1,1),
@@ -58,6 +79,7 @@ class MainFrame(wx.Frame):
             flag=wx.EXPAND
         )
 
+        # Build and add the Intake Status Panel.
         gbs.Add(
             self.intakeStatus,
             wx.GBPosition(1, 3),
@@ -65,6 +87,10 @@ class MainFrame(wx.Frame):
             flag=wx.EXPAND
         )
 
+        # Build and add the Intake Status Panel.
+        #
+        # Note: this should more than likely be absorbed into the driver assist
+        # panel since that is more relevant to it's context and purpose.
         gbs.Add(
             CameraButtonsPanel(mainPanel),
             wx.GBPosition(4, 1),
@@ -72,12 +98,14 @@ class MainFrame(wx.Frame):
             flag=wx.EXPAND
         )
 
+        # Define which columns of the sizer are growable.
         gbs.AddGrowableCol(0, 1)
         gbs.AddGrowableCol(1, 3)
         gbs.AddGrowableCol(2, 1)
         gbs.AddGrowableCol(3, 1)
-        gbs.AddGrowableRow(1, 1)
 
+        # Define which rows of the sizer are growable.
+        gbs.AddGrowableRow(1, 1)
         gbs.AddGrowableRow(2, 1)
 
         gbs.Fit(self)
